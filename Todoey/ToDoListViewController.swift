b39c524bb4889a4e5b633d31595265644b4b386d
mukulmahaur <mukulmahaur@gmail.com>
2019-04-07 14:42:15 +0530

    add new item to first screen done

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let array = ["one","two","three","four"]
+    var array = ["one","two","three","four"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +29,8 @@ class ToDoListViewController: UITableViewController {
     //TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(array[indexPath.row])
-        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){                    tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }else{
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
@@ -37,5 +38,23 @@ class ToDoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textFeild = UITextField()
+        
+        let alert = UIAlertController(title: "Add new ToDoey", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) {
+            (action) in
+            self.array.append(textFeild.text!)
+            self.tableView.reloadData()
+//            print(textFeild.text)
+        }
+        alert.addTextField { (alertTextfeild) in
+            alertTextfeild.placeholder = "Create new Todoey"
+            textFeild = alertTextfeild
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
